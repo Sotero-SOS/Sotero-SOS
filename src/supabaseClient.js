fa99bda471a20ed28c245fdc7c30cd064a8992c5
Joao-Victor17 <dorea.cupolo@gmail.com>
2025-09-23 03:08:33 -0300

    Remoção da arquitetura antiga: frontend

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://aymbrbqxvkpniojdlinv.supabase.co";
-const supabaseKey = "sb_publishable_I8bUArYc8KqV2wjfrXhezg_xTTwgK6I";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
